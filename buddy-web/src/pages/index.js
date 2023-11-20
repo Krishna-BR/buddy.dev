e0f9c1345b9981095f3796639df94a451f802dcc
Krishna-BR <62740264+Krishna-BR@users.noreply.github.com>
2023-11-20 10:14:42 +0000

    Added title

--- buddy-web/src/pages/index.js
+++ buddy-web/src/pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import  ReactMarkdown from 'react-markdown'
+import Head from 'next/head'
 
 const SYSTEM_MESSAGE ="You are a Buddy Bot, a helpful and versatile AI created by Krishna using state-of-art ML models and api's."
 
@@ -48,48 +49,53 @@ export default function Home() {
     console.log("Response JSON: ",responseJson)
   };
 
-  return <div className="flex flex-col h-screen">
-    {/* Navigation Bar */}
-    <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
-      <div className="text-xl font-bold">Buddy</div>
-      <div>
-        <input 
-          type="password" 
-          className="border p-1 rounded"
-          onChange={e=> setApiKey(e.target.value)}
-          value = {apiKey}
-          placeholder="Paste API Key here"/>
-      </div>
-    </nav>
-    {/* Message History */}
-    <div className="flex-1 overflow-y-scroll">
-      <div className="w-full max-w-screen-md mx-auto px-4">
-        {messages.filter(message => message.role !== "system").map((message, idx) => (
-          <div key={idx} className="my-3">
-            <div className="font-bold">
-              {message.role === "user" ? "You":"Buddy"}
-            </div>
-            <div className="text-lg prose">
-              <ReactMarkdown>{message.content}</ReactMarkdown>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-    {/* Message Input Box */}
+  return (
+  <><Head>
+  <title>Buddy - Your friendly neighborhood AI</title></Head>
+  <div className="flex flex-col h-screen">
+  {/* Navigation Bar */}
+  <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+    <div className="text-xl font-bold">Buddy</div>
     <div>
-      <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
-        <textarea 
-        className="border text-lg rounded-md p-1 flex-1" 
-        value = {userMessage}
-        onChange ={e => setUserMessage(e.target.value)}
-        rows={1}/>
-        <button 
-          className="border text-lg rounded-md p-1 w-20 ml-2 bg-blue-500 hover:bg-blue-600 text-white"
-          onClick={sendRequest}>
-          Send
-        </button>
-      </div>
+      <input 
+        type="password" 
+        className="border p-1 rounded"
+        onChange={e=> setApiKey(e.target.value)}
+        value = {apiKey}
+        placeholder="Paste API Key here"/>
+    </div>
+  </nav>
+  {/* Message History */}
+  <div className="flex-1 overflow-y-scroll">
+    <div className="w-full max-w-screen-md mx-auto px-4">
+      {messages.filter(message => message.role !== "system").map((message, idx) => (
+        <div key={idx} className="my-3">
+          <div className="font-bold">
+            {message.role === "user" ? "You":"Buddy"}
+          </div>
+          <div className="text-lg prose">
+            <ReactMarkdown>{message.content}</ReactMarkdown>
+          </div>
+        </div>
+      ))}
     </div>
   </div>
+  {/* Message Input Box */}
+  <div>
+    <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
+      <textarea 
+      className="border text-lg rounded-md p-1 flex-1" 
+      value = {userMessage}
+      onChange ={e => setUserMessage(e.target.value)}
+      rows={1}/>
+      <button 
+        className="border text-lg rounded-md p-1 w-20 ml-2 bg-blue-500 hover:bg-blue-600 text-white"
+        onClick={sendRequest}>
+        Send
+      </button>
+    </div>
+  </div>
+</div>
+</>
+  );
 }
